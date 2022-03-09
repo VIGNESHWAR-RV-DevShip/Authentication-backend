0c7c624d036d889c5dadcac88e448fc72b9e80b2
VIGNESHWAR-RV-DevShip <vigneshwar@devship.in>
2022-03-09 08:58:53 +0530

    added required endpoints

--- index.js
+++ index.js
@@ -33,15 +33,23 @@ app.post("/login",async (request,response)=>{
     const user = request.body;
 
     const existingUser = await client.db("devShip")
-                                     .collection("authenticated_Users")
-                                     .findOne({userName:user.userName});
+                                     .collection("authenticated_users")
+                                     .findOne({email:user.email});
 
     if(existingUser){
         const  passwordCheck = await bcrypt.compare(user.password,existingUser.password); 
 
         if(passwordCheck){
             const token = jwt.sign({id:existingUser._id},process.env.SECRET_KEY);
-            return response.status(200).send({id:(existingUser._id.toString()),token});
+
+            const update = await client.db("devShip")
+                                       .collection("authenticated_users")
+                                       .updateOne({_id:existingUser._id},
+                                                  {$set:{token:token}});
+           if(update){
+               return response.status(200).send({id:(existingUser._id.toString()),token});
+           }
+            
         }else{
             return response.status(400).send({message:"Invalid User"});
         }
@@ -51,7 +59,7 @@ app.post("/login",async (request,response)=>{
     }
 });
 
- app.post("/signUp", async (request,response)=>{
+ app.post("/signup", async (request,response)=>{
      const newUser = request.body;
 
      newUser.password = await generateHashedPassword(newUser.password);
@@ -69,34 +77,72 @@ app.post("/login",async (request,response)=>{
 
 app.get("/dashboard",user_Auth,async(request,response)=>{
        const id = request.header("id");
+       const token = request.header("token");
 
        const userInfo = await client.db("devShip")
-                                    .collection("authenticated_Users")
-                                    .findOne({_id:ObjectId(id)})
-                                    .projection({_id:0,firstName:1,lastName:1});
+                                    .collection("authenticated_users")
+                                    .findOne({_id:ObjectId(id),token:token},
+                                              {projection:{_id:0,firstName:1,lastName:1}})
 
        if(userInfo){
            return response.status(200).send(userInfo);
        }else{
         return response.status(400).send({message:"server busy,please try again later"});
        }
-
 })
 
   app.get("/profile",user_Auth,async(request,response)=>{
 
       const id= request.header("id");
+      const token = request.header("token");
 
       const userInfo = await client.db("devShip")
-                                   .collection("authenticated_Users")
-                                   .findOne({_id:ObjectId(id)})
-                                   .projection({_id:0,password:0});
+                                   .collection("authenticated_users")
+                                   .findOne({_id:ObjectId(id),token:token},
+                                             {projection:{_id:0,password:0,token:0}});
 
       if(userInfo){
           return response.status(200).send(userInfo);
       }else{
           return response.status(400).send({message:"server busy,please try again later"});
       }
+  });
+
+  app.post("/profile",user_Auth,async (request,response)=>{
+
+    const id= request.header("id");
+    const token = request.header("token");
+
+    const {firstName,lastName,email,gender} = request.body;
+
+    const updated = await client.db("devShip")
+                                 .collection("authenticated_users")
+                                 .updateOne({_id:ObjectId(id),token:token},
+                                           {$set:{firstName,lastName,email,gender}});
+
+    if(updated){
+        return response.status(200).send(updated);
+    }else{
+        return response.status(400).send({message:"server busy,please try again later"});
+    }
+      
+  })
+
+  app.post("/signout",user_Auth,async (request,response)=>{
+      
+      const id = request.header("id");
+      const token = request.header("token");
+
+      const signedOut = await client.db("devShip")
+                                   .collection("authenticated_users")
+                                   .updateOne({_id:ObjectId(id),token:token},{$unset:{token:""}});
+
+      if(signedOut){
+          return response.status(200).send(signedOut);
+      }else{
+          return response.status(400).send({message:"server busy,please try again later"});
+      }
+
   })
  
 
