c8e645eba8dee819f074be964de9a7516698f316
VIGNESHWAR-RV-DevShip <101089487+VIGNESHWAR-RV-DevShip@users.noreply.github.com>
2022-03-10 11:56:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -7,6 +8,7 @@ import {signupRoute} from "./controllers/signup.js";
 import { dashboardRoute } from "./controllers/dashboard.js";
 import { profileRoute } from "./controllers/profile.js";
 import { signoutRoute } from "./controllers/signout.js";
+import { productRoute } from "./controllers/product.js";
 
 
 dotenv.config();
@@ -14,6 +16,7 @@ dotenv.config();
 const app = express();
 
 const MongoURL = process.env.MongoDB_URL;
+const dbName = process.env.DB_NAME;
 const port = process.env.PORT;
 
 async function createConnection(){
@@ -22,6 +25,19 @@ async function createConnection(){
     return client;
 }
 export const client = await createConnection();
+
+async function createMongooseConnection(){
+  try{
+  const client = await mongoose.connect(MongoURL,{dbName});
+  console.log("database connected with mongoose");
+  return client;
+  }
+  catch(e){
+    console.log(e.message);
+  }
+}
+export const client2 = await createMongooseConnection();
+
 
 app.use(express.json());
 app.use(cors());
@@ -35,10 +51,8 @@ app.use("/login",loginRoute);
 
   app.use("/profile",profileRoute);
 
-  app.use("/profile",profileRoute);
-
   app.use("/signout",signoutRoute);
  
-
+  app.use("/products",productRoute);
 
 app.listen(port,console.log("server started at ",port));
